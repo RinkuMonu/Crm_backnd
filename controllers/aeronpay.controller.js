ce2b43c3cb34a110296e8647a9997dcf52d20634
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2026-02-09 13:04:15 +0530

    edit

--- controllers/aeronpay.controller.js
+++ controllers/aeronpay.controller.js
@@ -12,6 +12,8 @@ class aeronpay {
                 "client-secret": req.headers.client_secret,
                 "Content-Type": "application/json"
             }
+            console.log("aeronpay headers", headersData);
+        
             const response = await axios.post("https://superprodapi.aeronpay.in/api/core-services/serviceapi-prod/finance/securepay/v2/payout/imps_payment", data,
                 {
                     headers: headersData
